e28207a6b76835ffcea83ff8691e88b9d1b59d1e
Yassine Kacem <kacemyassine740@gmail.com>
2025-12-29 16:21:32 +0100

    eff

--- services-metier/MSNotification/src/main/java/tn/itbs/tp/repositories/AlerteRepo.java
+++
@@ -1,7 +0,0 @@
-package tn.itbs.tp.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.itbs.tp.models.Alerte;
-
-public interface AlerteRepo extends JpaRepository<Alerte, Long> {
-}
