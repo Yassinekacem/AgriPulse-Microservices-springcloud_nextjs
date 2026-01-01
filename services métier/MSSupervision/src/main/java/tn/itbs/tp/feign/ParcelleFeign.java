c9a1b1aca54aef84a277ef359df92796d3779474
Yassine Kacem <kacemyassine740@gmail.com>
2026-01-01 14:36:12 +0100

    resoudre probleme kafka

--- services métier/MSSupervision/src/main/java/tn/itbs/tp/feign/ParcelleFeign.java
+++ services métier/MSSupervision/src/main/java/tn/itbs/tp/feign/ParcelleFeign.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import tn.itbs.tp.models.Parcelle;
 
-@FeignClient(name = "MSExploitations", url = "http://localhost:9096/api/parcelles")
+@FeignClient(name = "MSExploitations")
 public interface ParcelleFeign {
 
-    @GetMapping("/{id}")
-    Parcelle getById(@PathVariable("id") int id);
+  @GetMapping("/api/parcelles/{id}")
+  Parcelle getById(@PathVariable("id") int id);
 }
+
