c65d393c552fddaa613ec497097dd0482e85930f
yassine kacem <110304629+Yassinekacem@users.noreply.github.com>
2026-01-06 17:48:19 +0100

    Update KafkaProducerService.java

--- services métier/MSSupervision/src/main/java/tn/itbs/tp/kafka/KafkaProducerService.java
+++ services métier/MSSupervision/src/main/java/tn/itbs/tp/kafka/KafkaProducerService.java
@@ -1,16 +1,14 @@
 package tn.itbs.tp.kafka;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducerService {
 
-    private final KafkaTemplate<String, AlerteEvent> kafkaTemplate;
-
-    public KafkaProducerService(KafkaTemplate<String, AlerteEvent> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
+    @Autowired
+    private KafkaTemplate<String, AlerteEvent> kafkaTemplate;
 
     public void envoyerAlerte(AlerteEvent event) {
         kafkaTemplate.send("alertes.supervision", event.getParcelleId().toString(), event);
